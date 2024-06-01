a76abb0fbf9357e7831fe5fb28b22c2303db2218
Clement <clement7903@hotmail.com>
2024-06-01 11:56:36 +0800

    update with new middlewares & working jwt token creation after user signed in

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,1 +1,57 @@
-const model = require('../models/categoryModel');
+const model = require("../models/categoryModel")
+
+var categoryController = {
+    selectAllCategories: (req, res, next) => {
+        const callback = (error, results, fields) => {
+            if (error) {
+                console.error("Error selectAllCategories:", error);
+                res.status(500).json(error);
+            }
+            else res.status(200).json(results);
+        }
+
+        model.selectAllCategories(callback);
+    },
+
+    createNewCategory: (req, res, next) => {
+        const data = {
+            name: req.body.name,
+            description: req.body.description
+        }
+
+        const callback = (error, results, fields) => {
+            if (error) {
+                console.error("Error createNewCategory:", error);
+                res.status(500).json(error);
+            } else {
+                res.status(201).json(results);
+            }
+        }
+
+        model.createNewCategory(data, callback);
+    },
+
+    selectByIdWithCategoryInfo: (req, res, next) => {
+        const data = {
+            catid: req.params.catid
+        }
+
+        const callback = (error, results, fields) => {
+            if (error) {
+                console.error("Error readCategoryById:", error);
+                res.status(500).json(error);
+            } else {
+                if (results.length == 0) {
+                    res.status(404).json({
+                        message: "Category not found"
+                    });
+                }
+                else res.status(200).json(results[0]);
+            }
+        }
+
+        model.selectByIdWithCategoryInfo(data, callback);
+    }
+}
+
+module.exports = categoryController;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const userController = require("../controllers/userController");
 const jwtMiddleware=require("../middlewares/jwtMiddleware");
 const router = express.Router();
+const controller = require('../controllers/userController');
 
+//create your routes eg router.get('/', controller.readAllUser);
+router.get("/", controller.getAllUser)
+router.post("/", controller.createNewUser)
+router.get("/:userid", controller.getUserById)
+router.put("/:userid", controller.updateUserById)
+router.delete("/:userid", controller.deleteUserById)
+router.post("/login", controller.loginUser, jwtMiddleware.generateToken, jwtMiddleware.sendToken) // use post because a jwt is being created, add in more middleware if needed (controller.loginUser, jwtMiddleware.generateToken....)
 
 module.exports = router;
